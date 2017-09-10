fb8acf0755baf7dd72e4262502dfc0030a8c6813
Coalminemerc8288 <coalminemerc8288@gmail.com>
2017-09-10 09:14:09 -0700

    power function

--- M1/src/com/pbhatna/stringandarrays/PowerFunction.java
+++ M1/src/com/pbhatna/stringandarrays/PowerFunction.java
@@ -5,29 +5,27 @@ package com.pbhatna.stringandarrays;
  */
 public class PowerFunction {
 
-    public static double pow(double value, int n) {
+    public static double myPow(double x, int n) {
         if (n == 0) {
             return 1;
         }
 
         if (n < 0) {
-            return 1 / pow(value, -n);
+            return 1 / (x * myPow(x, -(n + 1)));
         }
 
-        double result = pow(value, n/2);
-        System.out.println("result"+ result + "------>value" + n);
+        double result = myPow(x, n/2);
 
         if (n%2 == 0)
             return result * result;
         else
-            return value * result * result;
-}
+            return x * result * result;
+    }
 
     public static void main(String []args) {
         int n = -2147483648;
         double k = 2;
-        System.out.println("power of " + n +" for " + k + " is equal "+pow(k,n));
+        System.out.println("power of " + n +" for " + k + " is equal "+ myPow(k,n));
         System.out.println("should be equal to: "+ Math.pow(k,n));
     }
-
 }
