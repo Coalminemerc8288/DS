25e536e5ef9ecd32e60fe8b9c983fdf09b0abd83
Coalminemerc8288 <coalminemerc8288@gmail.com>
2017-10-02 21:12:23 -0700

    random

--- M1/src/com/pbhatna/stringandarrays/CheckPowerOfTwo.java
+++ M1/src/com/pbhatna/stringandarrays/CheckPowerOfTwo.java
@@ -17,6 +17,6 @@ public class CheckPowerOfTwo {
         return n == 1;
     }
     public static void main (String [] args) {
-        System.out.println(isPowerOfTwo(256));
+        System.out.println(isPowerOfTwo(258));
     }
 }
