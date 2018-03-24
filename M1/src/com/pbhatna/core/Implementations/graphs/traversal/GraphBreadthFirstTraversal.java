e70a3bf14ab1fc9d0f963c6218a466a729f7b876
Coal miner <priyankbhatnagar@Priyanks-MacBook-Pro.local>
2018-03-24 13:52:54 -0700

    graphs

--- M1/src/com/pbhatna/core/Implementations/graphs/traversal/GraphBreadthFirstTraversal.java
+++ M1/src/com/pbhatna/core/Implementations/graphs/traversal/GraphBreadthFirstTraversal.java
@@ -1,5 +1,6 @@
 package com.pbhatna.core.Implementations.graphs.traversal;
 
+import com.pbhatna.core.Implementations.graphs.impl.AdjacencyMatrixGraph;
 import com.pbhatna.core.Implementations.graphs.interfaces.Graph;
 
 import java.util.ArrayDeque;
@@ -7,6 +8,7 @@ import java.util.List;
 import java.util.Queue;
 
 public class GraphBreadthFirstTraversal {
+    private static int N = 8;
 
     public static void breadthFirstTraversal(Graph graph, int[] visited, int currentVertex) {
 
@@ -28,6 +30,27 @@ public class GraphBreadthFirstTraversal {
                     queue.add(v);
                 }
             }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Graph graph = new AdjacencyMatrixGraph(N, Graph.GraphType.DIRECTED);
+        graph.addEdge(1, 0);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 7);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(1, 5);
+        graph.addEdge(5, 6);
+        graph.addEdge(6, 3);
+        graph.addEdge(3, 4);
+
+        int[] visited = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
+
+        // This for-loop ensures that all nodes are covered even for an unconnected
+        // graph.
+        for (int i = 0; i < N; i++) {
+            breadthFirstTraversal(graph, visited, i);
         }
     }
 }
